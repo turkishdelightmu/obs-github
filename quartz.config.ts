632047876dfec0a793d47e264409cd847f6ac1b7
Ilayda <ilyada966@gmail.com>
2026-02-18 09:07:37 +0400

    vault backup: 2026-02-18 09:07:37

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "turkishdelightco.github.io/obs-github",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
